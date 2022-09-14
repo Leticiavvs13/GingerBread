97eb0cb04af1e8b8124828c529fb685dc00c2562
Leticiavvs13 <leticiavsvieira@gmail.com>
2022-09-13 22:16:46 -0300

    modelo detalhes

---
+++ app/src/main/java/br/com/etecia/gingerbread/ModeloDetalhes.java
@@ -0,0 +1,21 @@
+package br.com.etecia.gingerbread;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ModeloDetalhes extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_modelo_detalhes, container, false);
+        return v;
+    }
+}
